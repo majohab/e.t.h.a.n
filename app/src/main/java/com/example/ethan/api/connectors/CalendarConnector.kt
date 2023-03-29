64b2a3244275fa8ba5e3ae21cf755495c7e428cb
aschulte201 <a.schulte201@gmail.com>
2023-03-29 15:37:42 +0200

    temp save

---
+++ app/src/main/java/com/example/ethan/api/connectors/CalendarConnector.kt
@@ -0,0 +1,110 @@
+package com.example.ethan.api.connectors
+
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.JsonFactory
+import com.google.api.client.json.gson.GsonFactory
+import com.google.api.client.util.DateTime
+import com.google.api.client.util.store.FileDataStoreFactory
+import com.google.api.services.calendar.Calendar
+import com.google.api.services.calendar.CalendarScopes
+import org.json.JSONObject
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
+import java.io.*
+import java.security.GeneralSecurityException
+import java.util.*
+
+class CalendarConnector : AbstractConnector() {
+
+    override val url: String
+        get() = ""
+
+    override fun parseData(data: String): JSONObject {
+        TODO("Not yet implemented")
+    }
+
+    private val APPLICATION_NAME = "Google Calendar API Java Quickstart"
+
+    /**
+     * Global instance of the JSON factory.
+     */
+    private val JSON_FACTORY: JsonFactory = GsonFactory.getDefaultInstance()
+
+    /**
+     * Directory to store authorization tokens for this application.
+     */
+    private val TOKENS_DIRECTORY_PATH = "tokens"
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private val SCOPES: List<String> = Collections.singletonList(CalendarScopes.CALENDAR_READONLY)
+    private val CREDENTIALS_FILE_PATH = "/calendarAPICredentials.json"
+
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    @Throws(IOException::class)
+    private fun getCredentials(HTTP_TRANSPORT: NetHttpTransport): Credential? {
+        // Load client secrets.
+        val `in`: InputStream =
+            CalendarConnector::class.java.getResourceAsStream(CREDENTIALS_FILE_PATH)
+                ?: throw FileNotFoundException("Resource not found: $CREDENTIALS_FILE_PATH")
+        val clientSecrets =
+            GoogleClientSecrets.load(JSON_FACTORY, InputStreamReader(`in`))
+
+        // Build flow and trigger user authorization request.
+        val flow = GoogleAuthorizationCodeFlow.Builder(
+            HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES
+        )
+            .setDataStoreFactory(FileDataStoreFactory(File(TOKENS_DIRECTORY_PATH)))
+            .setAccessType("offline")
+            .build()
+        val receiver = LocalServerReceiver.Builder().setPort(8888).build()
+        //returns an authorized Credential object.
+        return AuthorizationCodeInstalledApp(flow, receiver).authorize("user")
+    }
+
+    @Throws(IOException::class, GeneralSecurityException::class)
+    fun execute() {
+        // Build a new authorized API client service.
+        val HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport()
+        val service = Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+            .setApplicationName(APPLICATION_NAME)
+            .build()
+
+        // List the next 10 events from the primary calendar.
+        val now = DateTime(System.currentTimeMillis())
+        val events: Events = service.events().list("primary")
+            .setMaxResults(10)
+            .setTimeMin(now)
+            .setOrderBy("startTime")
+            .setSingleEvents(true)
+            .execute()
+        val items: List<Event> = events.getItems()
+        if (items.isEmpty()) {
+            println("No upcoming events found.")
+        } else {
+            println("Upcoming events")
+            for (event in items) {
+                var start: DateTime = event.getStart().getDateTime()
+                if (start == null) {
+                    start = event.getStart().getDate()
+                }
+                System.out.printf("%s (%s)\n", event.getSummary(), start)
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/ethan/usecases/NavigationAssistance.kt
+++ app/src/main/java/com/example/ethan/usecases/NavigationAssistance.kt
@@ -1,18 +1,19 @@
 package com.example.ethan.usecases
 
-import com.example.ethan.api.connectors.HoroscopeConnector
+import com.example.ethan.api.connectors.CalendarConnector
 
 class NavigationAssistance : Thread() {
-    // private val horoscopeConnector = HoroscopeConnector()
+    private val calendarConnector = CalendarConnector()
 
     public override fun run(){
         print("Navigation Assistance started")
         while (true){
             execute()
+            Thread.sleep(1000 * 10)
         }
     }
 
     public fun execute(){
-        
+        calendarConnector.execute();
     }
 }
