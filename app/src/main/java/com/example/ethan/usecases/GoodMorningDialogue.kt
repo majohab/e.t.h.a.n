5771e73b12e188e211cfdfe980fb64b6b9970fc0
aschulte201 <a.schulte201@gmail.com>
2023-04-05 22:59:06 +0200

    preview for lecture

--- app/src/main/java/com/example/ethan/usecases/GoodMorningDialogue.kt
+++ app/src/main/java/com/example/ethan/usecases/GoodMorningDialogue.kt
@@ -1,5 +1,9 @@
 package com.example.ethan.usecases
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.ui.text.toLowerCase
+import com.example.ethan.api.connectors.CalendarConnector
 import com.example.ethan.api.connectors.HoroscopeConnector
 import com.example.ethan.api.connectors.NewsConnector
 import com.example.ethan.api.connectors.StocksConnector
@@ -9,12 +13,18 @@ class GoodMorningDialogue(onFinishedCallback: () -> Unit) : AbstractUseCase(onFi
     private var horoscopeConnector = HoroscopeConnector()
     private var newsConnector = NewsConnector()
     private var stocksConnector = StocksConnector()
+    private var calendarConnector = CalendarConnector()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun run() {
         println("GoodMorningDialogue Thread has been started!")
 
         // Request API 1
         val horoscopes = horoscopeConnector.get()
+
+        // Reqeuest API 0
+        val eventsFreeBusy = calendarConnector.get()["answer"]
+
         // Request API 2
         val news_json = newsConnector.get()
         val news_articles = news_json.getJSONArray("articles")
@@ -44,12 +54,23 @@ class GoodMorningDialogue(onFinishedCallback: () -> Unit) : AbstractUseCase(onFi
 
         // Greet user with all gather information
         speak("Good Morning. Today is the ${now.dayOfMonth} of ${now.month}. It is ${now.hour} o'clock and ${now.minute} minutes. ")
-        speak("You have 5 events for today. ")
+        speak("$eventsFreeBusy")
         speak("Here is your daily update for your preferred stocks: $stocknews_string. ")
         speak("This are your daily news: $news_string.")
 
         // Ask for his preferred transportation method
         askForUserVoiceInput("What is your favorite type of transportation for this day?")
+
+        if (lastUserVoiceInput.lowercase().contains("bus")) {
+            speak("You successfully set bus as your favourite transportation method for today.")
+        }else if(lastUserVoiceInput.lowercase().contains("train")){
+            speak("You successfully set train as your favourite transportation method for today.")
+        }else if(lastUserVoiceInput.lowercase().contains("bike")){
+            speak("You successfully set bike as your favourite transportation method for today.")
+        }else if(lastUserVoiceInput.lowercase().contains("foot")){
+            speak("You successfully set walking as your favourite transportation method for today.")
+        }
+        speak("Have a great day!")
         // Say how long it'll take the user to its destination
 
         println("GoodMorningDialogue Thread is about to end!")
