6f9928c9a63fda9080f24d893523dedb589f3782
LHENRIC <lasse.finn.henrich@gmail.com>
2023-04-06 14:44:31 +0200

    restructuring

---
+++ app/src/main/java/com/example/ethan/usecases/SocialAssistance.kt
@@ -0,0 +1,22 @@
+package com.example.ethan.usecases
+
+import android.os.Build
+import java.time.LocalDateTime
+
+class SocialAssistance(onFinishedCallback: () -> Unit) : AbstractUseCase(onFinishedCallback)  {
+    override fun getExecutionTime(): LocalDateTime {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            LocalDateTime.now()
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+    }
+
+    override fun initUseCase() {
+        TODO("Not yet implemented")
+    }
+
+    override fun executeUseCase() {
+        TODO("Not yet implemented")
+    }
+}
