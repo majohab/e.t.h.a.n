422d60f2427c2a276ef8c02aabd7dd7b3664ed62
aschulte201 <a.schulte201@gmail.com>
2023-04-15 20:42:25 +0200

    added preferencesDataStore, but Context needed

---
+++ app/src/main/java/com/example/ethan/preferences/IPreferencesAPI.kt
@@ -0,0 +1,10 @@
+package com.example.ethan.preferences
+
+import androidx.datastore.preferences.core.Preferences
+
+interface IPreferencesAPI {
+    suspend fun <T> getFirstPreference(key: Preferences.Key<T>,defaultValue: T):T
+    suspend fun <T> putPreference(key: Preferences.Key<T>,value:T)
+    suspend fun <T> removePreference(key: Preferences.Key<T>)
+    suspend fun <T> clearAllPreference()
+}
---
+++ app/src/main/java/com/example/ethan/preferences/PreferencesHelper.kt
@@ -0,0 +1,44 @@
+package com.example.ethan.preferences
+
+import android.content.Context
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.first
+
+private val Context.dataStore by preferencesDataStore(
+    name = "PreferenceDataStore"
+)
+
+class PreferencesHelper(context: Context) : IPreferencesAPI {
+
+    // dataSource access the DataStore file and does the manipulation based on our requirements.
+    private val dataSource = context.dataStore
+
+
+    /* This returns the last saved value of the key. If we change the value,
+        it wont effect the values produced by this function */
+    override suspend fun <T> getFirstPreference(key: Preferences.Key<T>, defaultValue: T) :
+            T = dataSource.data.first()[key] ?: defaultValue
+
+    // This Sets the value based on the value passed in value parameter.
+    override suspend fun <T> putPreference(key: Preferences.Key<T>, value: T) {
+        dataSource.edit {   preferences ->
+            preferences[key] = value
+        }
+    }
+
+    // This Function removes the Key Value pair from the datastore, hereby removing it completely.
+    override suspend fun <T> removePreference(key: Preferences.Key<T>) {
+        dataSource.edit { preferences ->
+            preferences.remove(key)
+        }
+    }
+
+    // This function clears the entire Preference Datastore.
+    override suspend fun <T> clearAllPreference() {
+        dataSource.edit { preferences ->
+            preferences.clear()
+        }
+    }
+}
