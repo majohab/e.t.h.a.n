fb7e8949ce82afb75ec252c0bbe109bde712df12
aschulte201 <a.schulte201@gmail.com>
2023-04-16 17:23:26 +0200

    push

--- app/src/main/java/com/example/ethan/usecases/LunchBreakConsultant.kt
+++ app/src/main/java/com/example/ethan/usecases/LunchBreakConsultant.kt
@@ -1,13 +1,47 @@
 package com.example.ethan.usecases
 
+import com.example.ethan.api.connectors.CalendarConnector
 import com.example.ethan.api.connectors.OpenStreetMapApi
 
 class LunchBreakConsultant(onFinishedCallback: () -> Unit) : AbstractUseCase(onFinishedCallback)  {
 
     override var resTimeID = "time_LBC"
     private var openStreetMapRestaurant = OpenStreetMapApi()
+    private var calendarConnector = CalendarConnector()
+
     override fun executeUseCase() {
+        var breakTime = 12
+        val timeOptions = mutableListOf<UserInputOption>()
+        for (i in 0..23) {
+            val option = UserInputOption(
+                tokens = listOf(i.toString()),
+                onSuccess = {
+                    breakTime = i
+                    println("") // DO NOT DELETE THIS LINE
+                }
+            )
+            timeOptions.add(option)
+        }
+        speakAndHearSelectiveInput(
+            question = "Hi. I'm here to assure you having the best break today. Around what hour do" +
+                    " prefer to eat something?", options = timeOptions
+        )
+
+        val eventsFreeBusy_json = calendarConnector.get()
+        val eventsTotal = eventsFreeBusy_json.getInt("total")
+
+        if (eventsTotal == 0){
+            // Preferred time is available
+        }else {
+            eventsFreeBusy_json.getJSONObject("events").keys().forEach {
+
+            }
+        }
+
         val restaurants = openStreetMapRestaurant.findNearestRestaurants(37.7749, -122.4194, 1000 , "italian")
         println(restaurants)
+
+        // get time slots >x minutes
+        // suggest restaurants that are in time
     }
 }
