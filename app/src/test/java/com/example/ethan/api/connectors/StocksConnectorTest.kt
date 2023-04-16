d5d90127e2467b8036e7809fd91b36d71c55a706
FredericFindeis <frederic.findeis@dhbw-engineering.de>
2023-04-16 19:13:51 +0200

    wrote stocks tests

---
+++ app/src/test/java/com/example/ethan/api/connectors/StocksConnectorTest.kt
@@ -0,0 +1,32 @@
+package com.example.ethan.api.connectors
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class StocksConnectorTest{
+
+    private lateinit var stocksConnector: StocksConnector
+
+    @Before
+    fun setUp(){
+        stocksConnector = StocksConnector()
+    }
+
+    @Test
+    fun getStocks(){
+        val stockslist_tickers = listOf("AAPL", "MSFT", "GOOG")
+        val stockslist_names = listOf("Apple", "Microsoft", "Alphabet")
+        var stocknews_string = ""
+        for (i in stockslist_tickers.indices)
+        {
+            val stocknews_json = stocksConnector.get(stockslist_tickers[i])
+            println(stocknews_json)
+            val stocknews_quote = stocknews_json.getJSONObject("Global Quote")
+            val price = stocknews_quote.optString("05. price").toFloat().toString()
+            stocknews_string += "Last price of " + stockslist_names[i] + " was $price$. "
+        }
+        assertNotEquals(null, stocknews_string)
+
+    }
+}
