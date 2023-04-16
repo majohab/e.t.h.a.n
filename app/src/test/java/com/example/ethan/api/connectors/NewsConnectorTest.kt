8a1c6c0ee109eff27a13f0ab284a22e570ecbc34
FredericFindeis <frederic.findeis@dhbw-engineering.de>
2023-04-16 17:37:59 +0200

    wrote unit tests

---
+++ app/src/test/java/com/example/ethan/api/connectors/NewsConnectorTest.kt
@@ -0,0 +1,27 @@
+package com.example.ethan.api.connectors
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class NewsConnectorTest{
+    private lateinit var newsConnector: NewsConnector
+
+    @Before
+    fun setUp(){
+        newsConnector = NewsConnector()
+    }
+
+    @Test
+    fun testString(){
+        assertEquals("https://newsapi.org/v2/top-headlines?country=us&apiKey=null",newsConnector.url)
+    }
+
+    @Test
+    fun parseData(){
+        val eventsFreeBusy_json = newsConnector.get()
+
+
+
+    }
+}
