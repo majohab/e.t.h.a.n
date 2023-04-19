736083c089eb4c4d0db032326c64b056276195e1
aschulte201 <a.schulte201@gmail.com>
2023-04-20 00:01:20 +0200

    fixed calendar connector tests

--- app/src/test/java/com/example/ethan/api/connectors/CalendarConnectorTest.kt
+++ app/src/test/java/com/example/ethan/api/connectors/CalendarConnectorTest.kt
@@ -4,13 +4,45 @@ import org.json.JSONObject
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.spy
+import org.mockito.Mockito.`when`
+
 
 class CalendarConnectorTest{
     private lateinit var calendarConnector: CalendarConnector
+    private lateinit var calendarConnectorParse: CalendarConnector
 
     @Before
     fun setUp(){
-        calendarConnector = CalendarConnector()
+        calendarConnector = spy(CalendarConnector())
+        calendarConnectorParse = spy(CalendarConnector())
+
+        val mockEvents = JSONObject()
+        val mockEventsSub = JSONObject()
+        val mockEventOne = JSONObject()
+        val mockEventTwo = JSONObject()
+
+        mockEventOne.put("startHour", 8)
+        mockEventOne.put("startMinute", 30)
+        mockEventOne.put("endHour", 12)
+        mockEventOne.put("endMinute", 0)
+        mockEventOne.put("location", "Lerchenstraße 1 Stuttgart")
+        mockEventsSub.put("1", mockEventOne)
+
+        mockEventTwo.put("startHour", 13)
+        mockEventTwo.put("startMinute", 0)
+        mockEventTwo.put("endHour", 15)
+        mockEventTwo.put("endMinute", 45)
+        mockEventTwo.put("location", "Lerchenstraße 1 Stuttgart")
+        mockEventsSub.put("2", mockEventTwo)
+
+        mockEvents.put("events", mockEventsSub)
+        mockEvents.put("total", 2)
+        mockEvents.put("nextEventID", 1)
+
+        `when`(calendarConnector.get()).thenReturn(mockEvents) // Mock implementation
+        `when`(calendarConnectorParse.get()).thenReturn(JSONObject()) // Mock implementation
     }
 
     @Test
@@ -20,17 +52,17 @@ class CalendarConnectorTest{
 
     @Test
     fun parseData(){
-        val eventsFreeBusy_json = calendarConnector.get()
-        val eventsTotal = eventsFreeBusy_json.getInt("total")
-        var eventsResponseString = ""
-
-
+        val eventsFreeBusy_json = calendarConnectorParse.get()
     }
 
 
     @Test
     fun getFirstPreferenceTest(){
         calendarConnector.getIdealExecutionTime(2, 5, 10)
+        calendarConnector.getIdealExecutionTime(12, 30, 60)
+        calendarConnector.getIdealExecutionTime(12, 30, 240)
+        calendarConnector.getIdealExecutionTime(11, 30, 60)
+        calendarConnector.getIdealExecutionTime(11, 30, 800)
     }
 
 
