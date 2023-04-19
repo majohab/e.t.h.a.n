be7ae36eaa975ddc611204fcf47aa8dbca9c660d
DerLasseHenrich <lasse.finn.henrich@gmail.com>
2023-04-19 10:35:59 +0200

    no preferences

--- app/src/main/java/com/example/ethan/preferences/PreferencesConstants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ethan.preferences
-
-import androidx.datastore.preferences.core.stringPreferencesKey
-
-object PreferenceDataStoreConstants {
-    val TRANSPORTATION = stringPreferencesKey("TRANSPORTATION_KEY")
-}
