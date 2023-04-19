51a65f78184e5c354a2ca57bdeb07974a575271e
FredericFindeis <frederic.findeis@dhbw-engineering.de>
2023-04-19 17:36:23 +0200

    tried gui test

--- app/src/test/java/com/example/ethan/ui/gui/GUITest.kt
+++ app/src/test/java/com/example/ethan/ui/gui/GUITest.kt
@@ -2,5 +2,12 @@ package com.example.ethan.ui.gui
 
 import org.junit.Assert.*
 import org.mockito.Mockito.*
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.mutableStateOf
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
-class GUITest
+class GUITest{
+
+
+}
